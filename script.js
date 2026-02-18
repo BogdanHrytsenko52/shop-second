d8b689acb2fc8dbb5b0bcac34e2566f1b803664c
BogdanHrytsenko52 <b.hrytsenko@ukma.edu.ua>
2026-02-18 19:57:05 +0000

    .

---
+++ script.js
@@ -0,0 +1,8 @@
+const DB_KEY = 'rewear_v6_db';
+
+const initialData = [
+    { id: 1, name: "HELMUT LANG 1999 COAT", price: 850, desc: "Iconic archival piece.", img: null },
+    /* ... і так далі весь JS код ... */
+];
+
+// ... класи App, Settings, Admin ...
